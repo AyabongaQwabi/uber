44072a379fae18111816a27dad69d898fffcb710
Onwaba <onwaba@projectcodex.co>
2015-08-26 13:04:50 +0200

    script to create database, and the tables

---
+++ sql/agent.sql
@@ -0,0 +1,8 @@
+create table agent (
+id int not null auto_increment,
+name varchar(50),
+last_name varchar(80),
+id_number varchar(16),
+employee_number varchar(13),
+primary key (id)
+)
---
+++ sql/issues.sql
@@ -0,0 +1,5 @@
+create table issues (
+id int not null auto_increment,
+issue varchar(200),
+primary key(id)
+)
